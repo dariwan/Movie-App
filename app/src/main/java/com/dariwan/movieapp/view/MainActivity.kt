4cbcd7eb24bb96bd74cc39a030230218b0dafde0
dariwan <dariwan1607@gmail.com>
2023-10-22 14:57:09 +0700

    first commit

---
+++ app/src/main/java/com/dariwan/movieapp/view/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.dariwan.movieapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.dariwan.movieapp.R
+import com.dariwan.movieapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupBottomNav()
+    }
+
+    private fun setupBottomNav() {
+        val navView: BottomNavigationView = binding.navView
+        navView.itemIconTintList = null
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        setupWithNavController(binding.navView, navController)
+    }
+}
